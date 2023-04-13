741c49213af28fe86faa30d4cd2666a6160e97ee
ljr0924 <13630461363@163.com>
2023-04-13 11:45:09 +0800

    修改测试文件

--- reflect_demo/reflect1_test.go
+++ reflect_demo/reflect1_test.go
@@ -1,18 +1,35 @@
 package reflect_demo
 
 import (
-    "fmt"
-    "reflect"
-    "testing"
+	"fmt"
+	"reflect"
+	"testing"
 )
 
 type X int
 
+type Order struct {
+	Id   int    `json:"id"`
+	Desc string `json:"desc"`
+}
+
 func TestReflect1(t *testing.T) {
 
-    var a X = 100
+	var a X = 100
 
-    ta := reflect.TypeOf(a)
-    fmt.Println(ta.Name(), ta.Kind())
+	ta := reflect.TypeOf(a)
+	va := reflect.ValueOf(a)
+
+	fmt.Println(ta.Name(), ta.Kind(), ta.String(), va)
+
+	var b = &Order{
+		Id:   1,
+		Desc: "desc",
+	}
+
+	tb := reflect.TypeOf(b)
+	vb := reflect.ValueOf(b)
+
+	fmt.Println(tb.Name(), tb.Kind(), tb.String(), tb.FieldAlign(), vb)
 
 }
